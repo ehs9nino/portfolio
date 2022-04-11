788f6f1ddb7963f6a710cdd7d118605a6a87f51d
Precious Akams <preciousakams@yahoo.com>
2022-04-11 15:32:01 +0100

    menuBar script added

---
+++ index.js
@@ -0,0 +1,8 @@
+function mobileMenuFunction() {
+  var menu = document.getElementById('menu-bar');
+  if(menu.style.display ==='block') {
+    menu.style.display = 'none';
+  } else{
+   menu.style.display = 'block'
+  }
+}
