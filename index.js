e9efa988be4171fdab80982d8bd141e720f5d467
Ehsan Qader <ehs9nino@gmail.com>
2022-04-11 22:02:07 +0500

    ES Linters add

---
+++ index.js
@@ -0,0 +1,8 @@
+function mobileMenuFunction() {
+  const menu = document.getElementsById('menu-bar');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+}
