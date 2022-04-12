c5458237819f511fe0351c1369151eb5d0092186
Ehsan Qader <ehs9nino@gmail.com>
2022-04-12 10:01:34 +0500

    DOM add for Open/Close Hamburger

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-function mobileMenuFunction() {
-  const menu = document.getElementsById('menu-bar');
-  if (menu.style.display === 'block') {
-    menu.style.display = 'none';
-  } else {
-    menu.style.display = 'block';
-  }
+function displayContent() {
+  document.querySelector('.container').style.display = 'none';
+  document.querySelector('.mobile-menu').style.display = 'block';
 }
+document.querySelector('.hamburger > img').getaddEventListener('click', displayContent);
+
+function closeMenu() {
+  document.querySelector('.container').style.display = 'block';
+  document.querySelector('.mobile-menu').style.display = 'none';
+}
+document.querySelector('.close').getaddEventListener('click', closeMenu);
+
+function portfolioSection() {
+  document.querySelector('.portfolio').location.asign = 'https://dashboard.microverse.org/progress';
+}
+document.querySelector('.portfolio').getaddEventListener('click', portfolioSection());
