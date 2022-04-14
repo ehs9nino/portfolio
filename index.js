947d299ae326147d10b844c114fcf2f25a08c5b3
Precious Akams <preciousakams@yahoo.com>
2022-04-14 18:06:53 +0100

    object creation

--- index.js
+++ index.js
@@ -26,3 +26,74 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.about').addEventListener('click', aboutSection);
   document.querySelector('.contact').addEventListener('click', contactSection);
 });
+// The object for dynamic Cards
+
+const projects = [{
+  // First Project
+  name: 'Profesional Art Printing Data',
+  discription: 'First A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry`s standard',
+  discriptionDesktop: 'Firsst Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  languages: ['html', 'bootstrap', 'Ruby'],
+  reference: ['https://en.wikipedia.org/wiki/HTML', 'https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework', 'https://en.wikipedia.org/wiki/Ruby_(programming_language'],
+},
+{
+  // second Project
+  name: 'Data Dashboard Healthcare',
+  discription: 'Second A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry`s standard',
+  discriptionDesktop: 'Second Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  languages: ['html', 'bootstrap', 'Ruby'],
+  reference: ['https://en.wikipedia.org/wiki/HTML', 'https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework', 'https://en.wikipedia.org/wiki/Ruby_(programming_language'],
+},
+{
+  // Third Project
+  name: 'Website Protfolio',
+  discription: 'Third A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry`s standard',
+  discriptionDesktop: 'Third Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  languages: ['html', 'bootstrap', 'Ruby'],
+  reference: ['https://en.wikipedia.org/wiki/HTML', 'https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework', 'https://en.wikipedia.org/wiki/Ruby_(programming_language'],
+
+},
+{
+  // Fourth Project
+  name: 'Profesional Art Printing Data',
+  discription: 'Fourth A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry`s standard',
+  discriptionDesktop: 'Fourth Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  languages: ['html', 'bootstrap', 'Ruby'],
+  reference: ['https://en.wikipedia.org/wiki/HTML', 'https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework', 'https://en.wikipedia.org/wiki/Ruby_(programming_language'],
+},
+{
+  // Fifth Project
+  name: 'Fifth Dashboard Healthcare',
+  discription: 'Fifth A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry`s standard',
+  discriptionDesktop: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  languages: ['html', 'bootstrap', 'Ruby'],
+  reference: ['https://en.wikipedia.org/wiki/HTML', 'https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework', 'https://en.wikipedia.org/wiki/Ruby_(programming_language'],
+},
+{
+  // sixth Project
+  name: 'Website Protfolio',
+  discription: 'Sixth A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry`s standard',
+  discriptionDesktop: 'Sixth Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+  languages: ['html', 'bootstrap', 'Ruby'],
+  reference: ['https://en.wikipedia.org/wiki/HTML', 'https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework', 'https://en.wikipedia.org/wiki/Ruby_(programming_language'],
+},
+];
+// creating generic elements
+function addElement(element, classes, parent) {
+  const newElem = document.createElement(element);
+  classes.forEach((cl) => newElem.classList.add(cl));
+  parent.appendChild(newElem);
+  return newElem;
+}
+// ==== Show-hide functions
+function show(toSHow) {
+  // Remove hide class and add show class
+  toSHow.classList.remove('hide');
+  toSHow.classList.add('show');
+}
+
+function hide(toHide) {
+  // Remove show class and add hide class
+  toHide.classList.remove('show');
+  toHide.classList.add('hide');
+}
